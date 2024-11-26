0d2179a6c733772ef14f772c445782ebc98dc6f9
Mr-Taufik <taozk4417@gmail.com>
2024-11-26 18:23:00 +0800

    Add files via upload

---
+++ test1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+
+    while (i <= 9) { // 外层循环：行
+        int j = 1;
+        while (j <= 9) { // 内层循环：列
+            printf("%d*%d=%2d\t", i, j, i * j);
+            j++;
+        }
+        printf("\n"); // 换行
+        i++;
+    }
+
+    return 0;
+}
