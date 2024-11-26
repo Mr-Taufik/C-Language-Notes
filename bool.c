a573b84347386ddd6f2ac41c357e25ce405f2572
Mr-Taufik <taozk4417@gmail.com>
2024-11-26 16:16:45 +0800

    Update and rename test1.c to bool.c

--- bool.c
+++ bool.c
@@ -1,3 +1,6 @@
+这个代码显示了布尔值的用法，主要用来熟悉bool在自定义函数里面的运用
+
+
 #include <stdio.h>
 #include <stdbool.h>
 
@@ -12,7 +15,7 @@ int main()
 		printf("Enter a significnat numbers");
 		scanf_s("%d", &numbers);
 	}
-	if (isPrime(numbers))
+	if (isPrime(numbers))/////这里调用了bool函数，bool函数会接受numbers并返回true or false
 	{
 		printf("This numbers is isPrime");
 	}
