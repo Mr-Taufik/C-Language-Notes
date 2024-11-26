23efae10fbc4aaf5d6a91ffd9b2c54a8aeece7df
Mr-Taufik <taozk4417@gmail.com>
2024-11-27 00:53:29 +0800

    Add files via upload

---
+++ test1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int sign = 1;
+	int sum = 0;
+
+	// 从 1 开始到 101，步长为 2
+	for (int i = 1; i <= 101; i += 2)
+	{
+		sum += i * sign; // 根据符号进行加减
+		sign = -sign;    // 每次符号交替
+	}
+
+	printf("plus number: %d\n", sum);
+	return 0;
+}
