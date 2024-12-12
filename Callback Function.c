794fde6608f0f1030cf16c0c9bf54c8349472cc0
Mr-Taufik <taozk4417@gmail.com>
2024-12-12 20:45:35 +0800

    Update and rename main.c to Callback Function.c

--- Callback Function.c
+++ Callback Function.c
@@ -3,15 +3,26 @@
 #include <stdio.h>
 
 typedef int(*T)(int ,int);
+
+//typedef int(*T)(int, int);: 定义了一个类型别名T，它表示一个指向函数的指针，
+//该函数接受两个int类型的参数并返回一个int。这使得在后续代码中使用函数指针更为简洁
+
 int great(int x, int y);
 int less(int x, int y);
 
 void sort(int *array, int length, int (*p)(int, int));
+
+//声明了一个sort函数，该函数用于对数组进行排序。它接受三个参数
+//int *array: 指向要排序的数组的指针。
+//int (*p)(int, int): 一个函数指针，指向用于比较数组元素的函数。
+
 int main()
 {
      T q1 = great;
      T q2 = less;
-
+     
+//使用之前定义的类型别名T，声明了两个函数指针q1和q2，分别指向great和less函数。这允许在排序时灵活地选择排序的方式（升序或降序）
+     
      int array[10];
 
      for(int i = 0; i < 10; i++)
